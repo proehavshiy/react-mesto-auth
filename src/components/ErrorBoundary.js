e8f96957cb4b2b3cb05e9544607dd09bf807cb94
Egor Kurkin <proehavshiynapravo@yandex.ru>
2021-07-07 17:25:16 +0300

    feat: Error Boundary testing but not successfully

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,48 +1,69 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+// class ErrorBoundary extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       hasError: false,
+//       error: null,
+//       errorInfo: null
+//     }
+//   }
+
+//   //Используйте static getDerivedStateFromError() при рендеринге запасного UI в случае отлова ошибки.
+//   static getDerivedStateFromError(error) {
+//     // Обновить состояние с тем, чтобы следующий рендер показал запасной UI.
+//     this.setState({
+//       hasError: true,
+//     })
+//     // return { hasError: true };
+//   }
+
+//   //Используйте componentDidCatch() при написании кода для журналирования информации об отловленной ошибке.
+//   componentDidCatch(error, errorInfo) {
+//     // Можно также сохранить информацию об ошибке в соответствующую службу журнала ошибок
+//     //но я выведу в консоль
+//     this.setState({
+//       error: error,
+//       errorInfo: errorInfo
+//     })
+//     console.log('error:', this.error);
+//     console.log('errorInfo:', this.errorInfo.componentStack);
+//   }
+
+//   render() {
+//     // const history = this.props.history;
+
+//     if (this.state.hasError) {
+//       return (
+//         <>
+//           <div>Возникла ошибка</div>
+//           {/* <NavLink onClick={() => history.goBack()}></NavLink> */}
+//         </>
+//       )
+//     };
+
+//     return this.props.children;
+//   }
+// }
+
 class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      hasError: false,
-      error: null,
-      errorInfo: null
-    }
+    this.state = { hasError: false };
   }
 
-  //Используйте static getDerivedStateFromError() при рендеринге запасного UI в случае отлова ошибки.
   static getDerivedStateFromError(error) {
-    // Обновить состояние с тем, чтобы следующий рендер показал запасной UI.
-    this.setState({
-      hasError: true,
-    })
-    // return { hasError: true };
-  }
-
-  //Используйте componentDidCatch() при написании кода для журналирования информации об отловленной ошибке.
-  componentDidCatch(error, errorInfo) {
-    // Можно также сохранить информацию об ошибке в соответствующую службу журнала ошибок
-    //но я выведу в консоль
-    this.setState({
-      error: error,
-      errorInfo: errorInfo
-    })
-    console.log('error:', error);
-    console.log('errorInfo:', errorInfo.componentStack);
+    // Обновите состояние так, чтобы следующий рендер показал запасной интерфейс.
+    return { hasError: true };
   }
 
   render() {
-    const history = this.props.history;
-
     if (this.state.hasError) {
-      return (
-        <>
-          <div>Возникла ошибка</div>
-          <NavLink onClick={() => history.goBack()}></NavLink>
-        </>
-      )
-    };
+      // Здесь можно рендерить запасной интерфейс
+      return <h1>Что-то пошло не так.</h1>;
+    }
 
     return this.props.children;
   }
